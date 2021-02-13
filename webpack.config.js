0a99d97036ccd66f806681e3d7edce77e978e319
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-13 07:25:06 -1000

    Webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
