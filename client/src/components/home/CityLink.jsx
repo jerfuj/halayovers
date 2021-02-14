668a8ef2ac4c27987aace4f0d7998b401f283994
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-13 22:16:10 -1000

    Receive city prop

--- client/src/components/home/CityLink.jsx
+++ client/src/components/home/CityLink.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import styles from './CityLink.module.css';
 
-const CityLink = ({city}) => {
+const CityLink = ({ city }) => {
   return (
     <div className={styles.linkContainer}>
-      <Link to={`/${city}`}>{city}</Link>
+      <Link to={`/${city.airport_code}`}>
+        <div className={styles.code}>{city.airport_code}</div>
+        <img src={city.img} className={styles.img} />
+      </Link>
     </div>
   )
 }
