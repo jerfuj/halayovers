32d47d8619a41a911439b60b3baf8c3644feb0fe
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-14 11:24:33 -1000

    Add ScrollToTop

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, useLocation } from 'react-router-dom';
 import Home from './home/Home.jsx';
 import City from './city/City.jsx';
 import styles from './App.module.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className={styles.app}>
         <div className={styles.innerApp}>
           <Switch>
