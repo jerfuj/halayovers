ac9eb627f27ad02c7638b9c75d446c02b3699d38
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-13 22:13:39 -1000

    Add get request for city and reviews data

--- client/src/components/city/City.jsx
+++ client/src/components/city/City.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, useParams} from 'react-router-dom';
+import $ from 'jquery';
 import CityHeader from './CityHeader.jsx';
 import ReviewList from './ReviewList.jsx';
 import styles from './City.module.css';
 
 const City = () => {
   let { id } = useParams();
+  const [city, setCity] = useState({});
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    $.ajax({
+      method: 'GET',
+      url: `http://localhost:3000/api/cities/${id}`,
+      success: (data) => {
+        console.log('DATA', data)
+        setCity(data[0][0]);
+        setReviews(data[1]);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }, [])
+
   return (
     <div className={styles.cityContainer}>
-      <CityHeader id={id} />
-      <ReviewList />
+      <CityHeader city={city} />
+      <ReviewList reviews={reviews} />
     </div>
   )
 }
