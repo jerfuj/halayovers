6fb8439b485e314bc6b53349fdfae2f5dbb65005
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-13 22:16:44 -1000

    Add useEffect, jquery

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import $ from 'jquery';
 import HomeHeader from './HomeHeader.jsx'
 import CityLink from './CityLink.jsx';
 import styles from './Home.module.css';
 
+const Home = ({ setSelectedCity }) => {
+  const [cities, setCities] = useState([])
 
-const cities = ['PHX', 'LGB', 'LAX', 'OAK', 'SMF', 'SAN', 'SFO', 'SJC', 'BOS', 'LAS', 'JFK', 'SEA', 'AUS', 'MCO', 'ONT', 'BNE', 'SYD', 'FUK', 'KIX', 'CTS', 'HND', 'NRT', 'AKL', 'ICN']
+  useEffect(() => {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:3000/api/cities',
+      success: (data) => {
+        setCities(data);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }, [])
 
-const Home = () => {
   return (
     <div className={styles.homeContainer}>
       <HomeHeader />
       <div className={styles.linkContainer}>
         {cities.map(city => (
-          <CityLink city={city} />
+          <CityLink city={city} setSelectedCity={setSelectedCity}/>
         ))}
       </div>
     </div>
