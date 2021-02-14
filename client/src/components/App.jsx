77f0644115e013d76a15337dfeee391336ca2e8d
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-13 22:12:31 -1000

    Add inner container

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './home/Home.jsx';
 import City from './city/City.jsx';
+import styles from './App.module.css';
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/:id">
-            <City />
-          </Route>
-        </Switch>
+      <div className={styles.app}>
+        <div className={styles.innerApp}>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/:id">
+              <City />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   )
