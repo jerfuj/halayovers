f6b8f591c9665706d20b9bb82e3f3e464c2eeac0
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-14 22:52:54 -1000

    Render review data

--- client/src/components/city/Review.jsx
+++ client/src/components/city/Review.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import dateFormat from 'dateformat';
+import styles from './Review.module.css';
 
 const Review = ({review}) => {
-
+  const categories = JSON.parse(review.categories);
+  const date = dateFormat(review.date, 'shortDate')
   return (
-    <p>
-      {review}
-    </p>
+    <li className={styles.reviewContainer}>
+      <div className={styles.nameAndDate} >
+        <div className={styles.name}>
+          {review.fa_name}
+        </div>
+        <div className={styles.date}>
+          {date}
+        </div>
+      </div>
+      <div className={styles.categories}>
+        <b>Category:</b> {categories.join(', ')}
+      </div>
+      <div className={styles.text}>
+        <b>Pro Tip:</b> {review.review_text}
+      </div>
+      <div className={styles.upvotes}>
+        <button>
+          useful {review.upvotes}
+        </button>
+      </div>
+    </li>
   )
 }
 
