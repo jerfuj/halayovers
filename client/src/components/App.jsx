d2ea2db4872264a92e9b3e9b84185d2cbe272a83
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-16 12:12:17 -1000

    Moved ajax and cities state here

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, useLocation } from 'react-router-dom';
+import $ from 'jquery';
 import Home from './home/Home.jsx';
 import City from './city/City.jsx';
 import styles from './App.module.css';
@@ -7,7 +8,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function ScrollToTop() {
+const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -18,23 +19,51 @@ function ScrollToTop() {
 }
 
 const App = () => {
+  const [cities, setCities] = useState([])
+
+  useEffect(() => {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:3000/api/cities',
+      success: (data) => {
+        setCities(data);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    })
+  }, [])
+
   return (
     <div>
-      <div className={styles.nav}>
-        <a href="/">
+      <Navbar bg="light" sticky="top">
+        <Navbar.Brand href="/">
           <img
+            alt=""
             src="https://bigbrain-itemdetails.s3-us-west-1.amazonaws.com/HA+Logo.png"
-            className={styles.logo}
-          />
-        </a>
-      </div>
+            height="50"
+            className="d-inline-block align-top"
+          />{' '}
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <NavDropdown title="Cities" id="basic-nav-dropdown">
+            {cities.map(city => (
+              <NavDropdown.Item href={`/${city.airport_code}`} className={styles.dropdownItem}>{city.airport_code}</NavDropdown.Item>
+            ))}
+          </NavDropdown>
+        </Nav>
+        <Navbar.Text>
+          <h3 className={styles.title}>Layover Info &amp; Tips</h3>
+        </Navbar.Text>
+      </Navbar>
       <Router>
         <ScrollToTop />
         <div className={styles.app}>
           <div className={styles.innerApp}>
             <Switch>
               <Route path="/" exact>
-                <Home />
+                <Home cities={cities}/>
               </Route>
               <Route path="/:id">
                 <City />
