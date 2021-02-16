ff52ec81309d9e5e220e838550d764b583fd9de3
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-15 21:36:41 -1000

    Add hotel component

---
+++ client/src/components/city/Hotel.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+import styles from './Hotel.module.css';
+
+const Hotel = ({ hotelInfo }) => {
+  if (!hotelInfo) {
+    return null;
+  }
+  return (
+    <div className={styles.tableContainer}>
+      <h2>Hotel Info</h2>
+      <Table striped className={styles.table}>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>{hotelInfo.name}</td>
+          </tr>
+          <tr>
+            <td>Address</td>
+            <td>{hotelInfo.address}</td>
+          </tr>
+          <tr>
+            <td>Phone Number</td>
+            <td>{hotelInfo.phone}</td>
+          </tr>
+          <tr>
+            <td>Microwave</td>
+            <td>{hotelInfo.microwave}</td>
+          </tr>
+          <tr>
+            <td>Refrigerator</td>
+            <td>{hotelInfo.fridge}</td>
+          </tr>
+          <tr>
+            <td>Breakfast</td>
+            <td>{hotelInfo.breakfast}</td>
+          </tr>
+          <tr>
+            <td>Gym</td>
+            <td>{hotelInfo.gym}</td>
+          </tr>
+          <tr>
+            <td>Shuttle</td>
+            <td>{hotelInfo.shuttle}</td>
+          </tr>
+          <tr>
+            <td>Pickup</td>
+            <td>{hotelInfo.pickup} prior to departure</td>
+          </tr>
+        </tbody>
+
+      </Table>
+    </div>
+  )
+}
+
+export default Hotel;
