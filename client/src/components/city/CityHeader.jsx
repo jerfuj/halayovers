07953be2bedd742d81e09d11d47c12abab45f7d4
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-15 21:33:28 -1000

    Change image to background image

--- client/src/components/city/CityHeader.jsx
+++ client/src/components/city/CityHeader.jsx
@@ -4,8 +4,14 @@ import styles from './CityHeader.module.css';
 const CityHeader = ({ city }) => {
   return (
     <div className={styles.headerContainer}>
-      <h1>Welcome To {city.city}, {city.state_or_country}!</h1>
-      <img className={styles.img} src={city.img}/>
+      <div
+        className={styles.imgContainer}
+        style={{
+          backgroundImage: `url(${city.img})`,
+          backgroundSize: 'cover'
+        }}>
+        <h1 className={styles.h1}>Welcome To {city.city}!</h1>
+      </div>
     </div>
   )
 }
