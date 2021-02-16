58f663f5fea95b52bf086c1e4c7062d00c5a6eb3
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-15 21:31:20 -1000

    Add navbar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link, useLocation } from 'react
 import Home from './home/Home.jsx';
 import City from './city/City.jsx';
 import styles from './App.module.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -16,21 +19,31 @@ function ScrollToTop() {
 
 const App = () => {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className={styles.app}>
-        <div className={styles.innerApp}>
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/:id">
-              <City />
-            </Route>
-          </Switch>
-        </div>
+    <div>
+      <div className={styles.nav}>
+        <a href="/">
+          <img
+            src="https://bigbrain-itemdetails.s3-us-west-1.amazonaws.com/HA+Logo.png"
+            className={styles.logo}
+          />
+        </a>
       </div>
-    </Router>
+      <Router>
+        <ScrollToTop />
+        <div className={styles.app}>
+          <div className={styles.innerApp}>
+            <Switch>
+              <Route path="/" exact>
+                <Home />
+              </Route>
+              <Route path="/:id">
+                <City />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </div>
   )
 }
 
