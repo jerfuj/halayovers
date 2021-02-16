626c22b8494541cbe76e4dabfd0afa5e3ae6d758
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-16 12:17:54 -1000

    Remove HomeHeader component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import $ from 'jquery';
-import HomeHeader from './HomeHeader.jsx'
 import CityLink from './CityLink.jsx';
 import styles from './Home.module.css';
 
@@ -8,7 +7,6 @@ const Home = ({ cities }) => {
 
   return (
     <div className={styles.homeContainer}>
-      <HomeHeader />
       <div className={styles.linkContainer}>
         {cities.map(city => (
           <CityLink city={city}/>
