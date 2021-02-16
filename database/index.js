23d5d3306f385ea3f8d6ede4c12302f46300d15a
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-15 21:35:52 -1000

    Implement sortReviews

--- database/index.js
+++ database/index.js
@@ -27,8 +27,29 @@ const getCities = (callback) => {
 }
 
 const getReviews = (id, callback) => {
-  connection.query('SELECT * FROM cities WHERE airport_code=?; SELECT * FROM reviews WHERE airport_code=?', [id, id], (err, data) => {
+  connection.query('SELECT * FROM cities WHERE airport_code=?; SELECT * FROM reviews WHERE airport_code=? ORDER BY date DESC; SELECT * FROM hotels WHERE airport_code=?', [id, id, id], (err, data) => {
     if (err) {
+      callback(err);
+    } else {
+      callback(err, data);
+    }
+  })
+}
+
+const sortReviews = (id, sort, callback) => {
+  let query;
+  if (sort === 'recent') {
+    query = `SELECT * FROM reviews WHERE airport_code=? ORDER BY date DESC`
+  }
+  if (sort === 'highestrated') {
+    query = `SELECT * FROM reviews WHERE airport_code=? ORDER BY upvotes DESC`
+  }
+  if (sort === 'oldest') {
+    query = `SELECT * FROM reviews WHERE airport_code=? ORDER BY date ASC`
+  }
+  connection.query(query, [id], (err, data) => {
+    if (err) {
+      console.log(err);
       callback(err);
     } else {
       callback(err, data);
@@ -52,8 +73,20 @@ const postReview = (id, reviewData, callback) => {
   })
 }
 
+const upVote = (id, callback) => {
+  connection.query('UPDATE reviews SET upvotes = upvotes + 1 WHERE id = ?', [id], (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, data);
+    }
+  })
+}
+
 module.exports = {
   getCities,
   getReviews,
-  postReview
+  postReview,
+  upVote,
+  sortReviews
 }
