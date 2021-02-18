04082a9398e453ffbd211440e95143ea018fcb51
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-02-17 17:52:00 -1000

    Complete sorting functionality

--- client/src/components/city/ReviewList.jsx
+++ client/src/components/city/ReviewList.jsx
@@ -11,6 +11,9 @@ const ReviewList = ({ city, reviews, getCityReviews, hotelInfo, changeSort }) =>
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [sortOption, setSortOption] = useState('recent')
+  const [categorySort, setCategorySort] = useState(null);
+
   return (
     <div className={styles.reviewsContainer}>
       <Hotel hotelInfo={hotelInfo}/>
@@ -22,17 +25,94 @@ const ReviewList = ({ city, reviews, getCityReviews, hotelInfo, changeSort }) =>
       />
       <div className={styles.btnAndSortContainer}>
         <Button variant="outline-dark" onClick={handleShow}>Write a Review!</Button>
-        <Form.Control as="select" className={styles.select} onChange={(e) => {
-          changeSort(e, city.airport_code);
-        }}>
+        <Form.Control
+          as="select"
+          className={styles.select}
+          onChange={(e) => {
+            setSortOption(e.target.value);
+            changeSort(e, city.airport_code, e.target.value, categorySort);
+          }}
+        >
           <option value="recent">Most Recent</option>
           <option value="highestrated">Highest Rated</option>
           <option value="oldest">Oldest</option>
         </Form.Control>
       </div>
+      <div className={styles.filterCategory}>
+        <Button
+          onClick={(e) => {
+            setCategorySort(e.target.value);
+            changeSort(e, city.airport_code, sortOption, e.target.value)
+          }}
+          className={styles.btn}
+          value="null"
+        >
+          All
+        </Button>
+        <Button
+          onClick={(e) => {
+            setCategorySort(e.target.value);
+            changeSort(e, city.airport_code, sortOption, e.target.value)
+          }}
+          className={styles.btn}
+          value="Food"
+        >
+          Food
+        </Button>
+        <Button
+          onClick={(e) => {
+            setCategorySort(e.target.value);
+            changeSort(e, city.airport_code, sortOption, e.target.value)
+          }}
+          className={styles.btn}
+          value="Sightseeing"
+        >
+          Sightseeing
+        </Button>
+        <Button
+          onClick={(e) => {
+            setCategorySort(e.target.value);
+            changeSort(e, city.airport_code, sortOption, e.target.value)
+          }}
+          className={styles.btn}
+          value="Hotel"
+        >
+          Hotel
+        </Button>
+        <Button
+          onClick={(e) => {
+            setCategorySort(e.target.value);
+            changeSort(e, city.airport_code, sortOption, e.target.value)
+          }}
+          className={styles.btn}
+          value="Airport"
+        >
+          Airport
+        </Button>
+        <Button
+          onClick={(e) => {
+            setCategorySort(e.target.value);
+            changeSort(e, city.airport_code, sortOption, e.target.value)
+          }}
+          className={styles.btn}
+          value="Discounts"
+        >
+          Discounts
+        </Button>
+        <Button
+          onClick={(e) => {
+            setCategorySort(e.target.value);
+            changeSort(e, city.airport_code, sortOption, e.target.value)
+          }}
+          className={styles.btn}
+          value="Misc"
+        >
+          Misc
+        </Button>
+      </div>
       <ul className={styles.list}>
         {reviews.map(review => (
-          <Review review={review} getCityReviews={getCityReviews}/>
+          <Review key={review.id} review={review} getCityReviews={getCityReviews}/>
         ))}
       </ul>
     </div>
