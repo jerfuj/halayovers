cf0072483ebf2524b3b0ec69e4b1f10d4e4ff01c
Jeremy Fujimoto <jeremyfujimoto@gmail.com>
2021-03-02 19:55:01 -1000

    Add demo modal

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import CityLink from './CityLink.jsx';
 import styles from './Home.module.css';
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
 const Home = ({ cities }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, [])
 
   return (
     <div className={styles.homeContainer}>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+          <h4>Aloha!</h4>
+          <p>
+            For best demo experience, head over to KIX! Thanks for looking!
+
+            - Jeremy
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
       <div className={styles.linkContainer}>
         {cities.map(city => (
           <CityLink key={city.airport_code} city={city}/>
